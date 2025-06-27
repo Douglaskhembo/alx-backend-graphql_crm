d294476fccc3c3cacfcc04b2100752a785f16ff9
khembo <khembo.douglas.juma@gmail.com>
2025-06-27 23:29:38 +0300

    Task 2: Implement Complex GraphQL Mutations for CRM

--- alx_backend_graphql_crm/urls.py
+++ alx_backend_graphql_crm/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
-from .schema import schema  # <-- Add this line
+from ..crm.schema import schema  # <-- Add this line
 
 urlpatterns = [
     path('admin/', admin.site.urls),
---
+++ crm/schema.py
@@ -0,0 +1,111 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Customer, Product, Order
+from graphql import GraphQLError
+
+class CustomerType(DjangoObjectType):
+    class Meta:
+        model = Customer
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+
+class OrderType(DjangoObjectType):
+    class Meta:
+        model = Order
+
+class CustomerInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
+    email = graphene.String(required=True)
+    phone = graphene.String()
+
+class ProductInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
+    price = graphene.Float(required=True)
+    stock = graphene.Int(required=True)
+
+class OrderInput(graphene.InputObjectType):
+    customer_id = graphene.ID(required=True)
+    product_ids = graphene.List(graphene.ID, required=True)
+
+class CreateCustomer(graphene.Mutation):
+    class Arguments:
+        input = CustomerInput(required=True)
+
+    customer = graphene.Field(CustomerType)
+    message = graphene.String()
+
+    def mutate(self, info, input):
+        customer = Customer.objects.create(**input)
+        return CreateCustomer(customer=customer, message="Customer created successfully")
+
+
+class BulkCreateCustomers(graphene.Mutation):
+    class Arguments:
+        input = graphene.List(CustomerInput, required=True)
+
+    customers = graphene.List(CustomerType)
+    errors = graphene.List(graphene.String)
+
+    def mutate(self, info, input):
+        customers = []
+        errors = []
+        for data in input:
+            try:
+                customer = Customer.objects.create(**data)
+                customers.append(customer)
+            except Exception as e:
+                errors.append(str(e))
+        return BulkCreateCustomers(customers=customers, errors=errors)
+
+
+class CreateProduct(graphene.Mutation):
+    class Arguments:
+        input = ProductInput(required=True)
+
+    product = graphene.Field(ProductType)
+
+    def mutate(self, info, input):
+        product = Product.objects.create(**input)
+        return CreateProduct(product=product)
+
+
+class CreateOrder(graphene.Mutation):
+    class Arguments:
+        input = OrderInput(required=True)
+
+    order = graphene.Field(OrderType)
+
+    def mutate(self, info, input):
+        try:
+            customer = Customer.objects.get(pk=input.customer_id)
+            products = Product.objects.filter(id__in=input.product_ids)
+
+            total = sum(p.price for p in products)
+            order = Order.objects.create(customer=customer, total_amount=total)
+            order.products.set(products)
+            return CreateOrder(order=order)
+        except Exception as e:
+            raise GraphQLError(str(e))
+
+class Query(graphene.ObjectType):
+    all_customers = graphene.List(CustomerType)
+    all_products = graphene.List(ProductType)
+    all_orders = graphene.List(OrderType)
+
+    def resolve_all_customers(root, info):
+        return Customer.objects.all()
+
+    def resolve_all_products(root, info):
+        return Product.objects.all()
+
+    def resolve_all_orders(root, info):
+        return Order.objects.all()
+
+
+class Mutation(graphene.ObjectType):
+    create_customer = CreateCustomer.Field()
+    bulk_create_customers = BulkCreateCustomers.Field()
+    create_product = CreateProduct.Field()
+    create_order = CreateOrder.Field()
