890a318f34f2164b10394fe7e7d84f7a13e1df42
khembo <khembo.douglas.juma@gmail.com>
2025-07-12 15:07:07 +0300

    Celery Task for Generating CRM Reports

--- crm/settings.py
+++ crm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'graphene_django',
     'crm',
     'django_crontab',
+    'django_celery_beat',
 ]
 
 INSTALLED_APPS += ['django_crontab']
@@ -136,3 +138,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
